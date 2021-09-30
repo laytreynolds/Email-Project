6e617a9aabfdd19e45fd2c9ef7758fbff49f02af
laytreynolds <laytreynolds@hotmail.com>
2021-09-30 19:56:51 +0100

    Initialise Email Application

---
+++ EmailApplication/src/main/java/com/qa/emailapplication/EmailApplication.java
@@ -0,0 +1,13 @@
+package com.qa.emailapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApplication.class, args);
+	}
+
+}
