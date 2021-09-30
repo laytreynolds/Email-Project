d0a9377adfa158d6cb2b4708510a5aa9d117ae61
laytreynolds <laytreynolds@hotmail.com>
2021-09-30 20:01:09 +0100

    Declared Variables, Getters and Setters

---
+++ EmailApplication/src/main/java/com/qa/emailapplication/Email.java
@@ -0,0 +1,72 @@
+package com.qa.emailapplication;
+
+public class Email {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;	
+	private String email;	
+	private int mailboxCapacity = 500;
+	private int defaultPasswordLength = 10;
+	private String alternateEmail;
+	private String companySuffix = "uber.com";
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getMailboxCapacity() {
+		return mailboxCapacity;
+	}
+	public void setMailboxCapacity(int mailboxCapacity) {
+		this.mailboxCapacity = mailboxCapacity;
+	}
+	public int getDefaultPasswordLength() {
+		return defaultPasswordLength;
+	}
+	public void setDefaultPasswordLength(int defaultPasswordLength) {
+		this.defaultPasswordLength = defaultPasswordLength;
+	}
+	public String getAlternateEmail() {
+		return alternateEmail;
+	}
+	public void setAlternateEmail(String alternateEmail) {
+		this.alternateEmail = alternateEmail;
+	}
+	public String getCompanySuffix() {
+		return companySuffix;
+	}
+	public void setCompanySuffix(String companySuffix) {
+		this.companySuffix = companySuffix;
+	}
+	
+	
+	
+
+}
