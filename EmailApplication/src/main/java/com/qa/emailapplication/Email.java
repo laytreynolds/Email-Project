aaf9e8739dae79f764a7982db2312d58b053de5f
laytreynolds <laytreynolds@hotmail.com>
2021-09-30 21:09:06 +0100

    Finalise Email Variables

--- EmailApplication/src/main/java/com/qa/emailapplication/Email.java
+++ EmailApplication/src/main/java/com/qa/emailapplication/Email.java
@@ -1,7 +1,17 @@
 package com.qa.emailapplication;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Email {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	private String firstName;
 	private String lastName;
 	private String password;
